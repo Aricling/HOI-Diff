4ad4a8155aa802c98d889cf75a8edd3ebae66c30
Aricling <aricling69@gmail.com>
2024-05-19 22:55:33 -0400

    correct train and test.txt

--- train_APDM.sh
+++ train_APDM.sh
@@ -1,7 +1,8 @@
-python -m train.train_affordance \ 
-    --save_dir ./save/afford_pred \ 
-    --dataset behave \ 
-    --save_interval 1000 \ 
-    --num_steps 20000 \ 
-    --batch_size 32 \ 
+python -m train.train_affordance \
+    --save_dir ./save/afford_pred \
+    --overwrite \
+    --dataset behave \
+    --save_interval 1000 \
+    --num_steps 20000 \
+    --batch_size 32 \
     --diffusion_steps 500
